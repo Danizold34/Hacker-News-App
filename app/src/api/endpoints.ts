57bf9106b0ca1fb8617bc3b4a1e9e4897d83c472
Dovlet Jumaev <Danizold34@gmail.com>
2023-04-08 02:37:53 +0300

    add minor changes

--- app/src/api/endpoints.ts
+++
@@ -1,7 +0,0 @@
-export const endpoints = {
-  getNewestNewsIds: () => 'https://hacker-news.firebaseio.com/v0/newstories.json',
-  getNewByIds: (id: number) =>
-    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
-  getComments: (id: number) =>
-    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
-}
